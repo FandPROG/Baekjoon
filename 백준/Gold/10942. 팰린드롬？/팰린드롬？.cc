1276cdd37bb38210161cb682c333ad34612a4081
FandPROG <88233473+FandPROG@users.noreply.github.com>
2024-03-02 15:48:42 +0900

    [Gold IV] Title: 팰린드롬?, Time: 320 ms, Memory: 16904 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[2010];
+int dp[2010][2010];
+
+int main()
+{
+	int i, j;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) scanf("%d", &arr[i]);
+	for (i = 1; i <= n; i++) dp[i][i] = 1;
+	for (i = 1; i < n; i++) if (arr[i] == arr[i + 1]) dp[i][i + 1] = 1;
+	for (i = n; i >= 1; i--) {
+		for (j = i + 1; j <= n; j++) {
+			if (arr[i] == arr[j] && dp[i + 1][j - 1] == 1) dp[i][j] = 1;
+		}
+	}
+	scanf("%d", &m);
+	for (i = 0; i < m; i++) {
+		int x, y;
+		scanf("%d %d", &x, &y);
+		if (dp[x][y] > 0) printf("1\n");
+		else printf("0\n");
+	}
+	return 0;
+}

19edf570c258ca7401fc47a71e1b1a57ca66278c
FandPROG <88233473+FandPROG@users.noreply.github.com>
2024-03-02 17:03:57 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 1360 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,33 +1,41 @@
 #include <stdio.h>
+#include <algorithm>
+#include <vector>
 
-int n;
-long long k, arr[1000010];
+using namespace std;
 
-long long pandas(long long mid)
+long long n, k;
+vector <long long> vec;
+
+long long pand(long long x)
 {
-    int i;
-    long long ans = 0;
-    for (i = 0; i < n; i++) {
-        ans += arr[i] / mid;
-    }
-    return ans;
+	long long i, j;
+	long long ans = 0;
+	for (i = 0; i < n; i++) {
+		if (vec[i] >= x) ans += (vec[i] / x);
+	}
+	return ans;
 }
 
 int main()
 {
-    int i, j;
-    scanf("%d %lld", &n, &k);
-    long long low = 1, high = -2e9;
-    for (i = 0; i < n; i++) {
-        scanf("%lld", &arr[i]);
-        if (arr[i] > high) high = arr[i] + 1;
-    }
-    while (low <= high) {
-        long long mid = (low + high) / 2;
-        long long mid_res = pandas(mid);
-        if (mid_res < k) high = mid - 1;
-        else low = mid + 1;
-    }
-    printf("%lld", high);
-    return 0;
+	long long i, j;
+	scanf("%lld %lld", &n, &k);
+	long long mx = -2e9;
+	for (i = 0; i < n; i++) {
+		long long x;
+		scanf("%lld", &x);
+		vec.push_back(x);
+		if (mx < x) mx = x;
+	}
+	long long low = 1, high = mx;
+	while (low <= high)
+	{
+		long long mid = (low + high) / 2;
+		long long mid_res = pand(mid);
+		if (mid_res >= k) low = mid + 1;
+		else high = mid - 1;
+	}
+	printf("%lld", high);
+	return 0;
 }

422ad51e83031e5610ffd6567c957c433aceeb22
FandPROG <88233473+FandPROG@users.noreply.github.com>
2024-03-03 16:10:18 +0900

    [Gold III] Title: 줄 세우기, Time: 40 ms, Memory: 3028 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,32 +1,37 @@
 #include <stdio.h>
-#include <vector>
 #include <queue>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int in[35000];
-vector<int> v[35000];
-queue<int> q;
+int n, m;
+int ent[32010];
+vector <int> vec[32010];
 
 int main()
 {
-	int i, j, n, m, a, b;
+	int i, j;
 	scanf("%d %d", &n, &m);
-	for (int i = 0; i < m; i++) {
-		scanf("%d %d", &a, &b);
-		in[b]++;
-		v[a].push_back(b);
+	for (i = 0; i < m; i++) {
+		int x, y;
+		scanf("%d %d", &x, &y);
+		vec[x].push_back(y);
+		ent[y]++;
 	}
+	queue <int> q;
 	for (i = 1; i <= n; i++) {
-		if (in[i] == 0) q.push(i);
+		if (ent[i] == 0) q.push(i);
 	}
 	while (!q.empty()) {
-		i = q.front();
+		int now = q.front();
 		q.pop();
-		printf("%d ", i);
-		for (j = 0; j < v[i].size(); j++) {
-			if (--in[v[i][j]] == 0) q.push(v[i][j]);
+		printf("%d ", now);
+		for (i = 0; i < vec[now].size(); i++) {
+			int idx = vec[now][i];
+			ent[idx]--;
+			if (ent[idx] <= 0) {
+				q.push(idx);
+			}
 		}
 	}
 	return 0;

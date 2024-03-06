be21243218da36e023b4c460e7dd43b53e27d9f0
FandPROG <88233473+FandPROG@users.noreply.github.com>
2024-03-06 22:48:53 +0900

    [Gold II] Title: 책정리, Time: 40 ms, Memory: 3556 KB -BaekjoonHub

---
+++ 백준/Gold/1818. 책정리/책정리.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n, arr[200010];
+
+int main()
+{
+    int i, j;
+    scanf("%d", &n);
+    for(i=0;i<n;i++) scanf("%d", &arr[i]);
+    vector<int> lis;
+    lis.push_back(arr[0]);
+    for(i=1;i<n;i++) {
+        int lb = lower_bound(lis.begin(), lis.end(), arr[i]) - lis.begin();
+        int sz = lis.size();
+        if(lb >= sz) lis.push_back(arr[i]);
+        else lis[lb] = arr[i];
+    }
+    printf("%d", n - lis.size());
+    return 0;
+}

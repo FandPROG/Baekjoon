3daaed35429a170ac828d793be6c3c12122b07b4
FandPROG <88233473+FandPROG@users.noreply.github.com>
2024-03-08 20:40:21 +0900

    [Platinum III] Title: 정사각형 만들기, Time: 12 ms, Memory: 5556 KB -BaekjoonHub

--- 백준/Platinum/10803. 정사각형 만들기/정사각형 만들기.cc
+++ 백준/Platinum/10803. 정사각형 만들기/정사각형 만들기.cc
@@ -8,20 +8,19 @@ int n, m;
 
 int dp[10010][110];
 
-int Div(int x, int y) 
+int Div(int x, int y)
 {
 	int i, j;
 	if (x == y) return dp[x][y] = 1;
 	if (x < 0 || y < 0) return 2e9;
-	int& ans = dp[x][y];
-	if (ans != -1) return ans;
-	ans = x * y;
-	if (x >= 3 * y) ans = min(ans, Div(x - y, y) + 1);
+	if (dp[x][y] != -1) return dp[x][y];
+	dp[x][y] = x * y;
+	if (x >= 3 * y) dp[x][y] = min(dp[x][y], Div(x - y, y) + 1);
 	else {
-		for (i = 1; i <= (y + 1) / 2; i++) ans = min(ans, Div(x, i) + Div(x, y - i));
-		for (i = 1; i <= (x + 1) / 2; i++) ans = min(ans, Div(i, y) + Div(x - i, y));
+		for (i = 1; i <= (y + 1) / 2; i++) dp[x][y] = min(dp[x][y], Div(x, i) + Div(x, y - i));
+		for (i = 1; i <= (x + 1) / 2; i++) dp[x][y] = min(dp[x][y], Div(i, y) + Div(x - i, y));
 	}
-	return ans;
+	return dp[x][y];
 }
 
 int main()

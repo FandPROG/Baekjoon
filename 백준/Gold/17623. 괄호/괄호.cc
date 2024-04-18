2e8a0f62d65201a6bc6dd8bc31f32e1d0776f6ec
FandPROG <88233473+FandPROG@users.noreply.github.com>
2024-04-18 21:31:17 +0900

    [Gold II] Title: 괄호, Time: 60 ms, Memory: 2056 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17623. 괄호/괄호.cc
+++ 백준/Gold/17623. 괄호/괄호.cc
@@ -1,77 +1,37 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <string>
-#include <map>
-#include <cmath>
 
 using namespace std;
 
+int n, k;
 string dp[1010];
-map <char, int> mp;
+char d_s[15] = { ' ', '(', ')', '{', '}', '[', ']'};
 
-unsigned long long Sres(string s)
+string compare(string a, string b)
 {
-	int i, j = s.size() - 1;
-	unsigned long long res = 0;
-	for (i = 0; i < s.size(); i++, j--) {
-		res += mp[s[i]] * pow(10, j);
-	}
-	return res;
+	if (a.size() == 0 || a.size() > b.size()) return b;
+	if (b.size() > a.size()) return a;
+	return min(a, b);
 }
 
 int main()
 {
-	int i, j;
-	int t;
-	cin >> t;
-	dp[1] = "()";
-	dp[2] = "{}";
-	dp[3] = "[]";
-	mp.insert({ '(', 1 });
-	mp.insert({ ')', 2 });
-	mp.insert({ '{', 3 });
-	mp.insert({ '}', 4 });
-	mp.insert({ '[', 5 });
-	mp.insert({ ']', 6 });
+	int t, i, j;
+	scanf("%d", &t);
+	dp[1] = "12";
+	dp[2] = "34";
+	dp[3] = "56";
 	for (i = 4; i <= 1000; i++) {
-		unsigned long long imsi;
-		dp[i] = "()" + dp[i - 1];
-
-		if (i % 2 == 0) {
-			imsi = Sres("(" + dp[i / 2] + ")");
-			if (imsi < Sres(dp[i])) dp[i] = "(" + dp[i / 2] + ")";
-		}
-
-		if (i % 3 == 0) {
-			imsi = Sres("{" + dp[i / 3] + "}");
-			if (imsi < Sres(dp[i])) dp[i] = "{" + dp[i / 3] + "}";
-		}
-
-		if (i % 5 == 0) {
-			imsi = Sres("[" + dp[i / 5] + "]");
-			if (imsi < Sres(dp[i])) dp[i] = "[" + dp[i / 5] + "]";
-		}
-
-		imsi = Sres(dp[i - 1] + "()");
-		if (imsi < Sres(dp[i])) dp[i] = dp[i - 1] + "()";
-
-		imsi = Sres(dp[i - 2] + "{}");
-		if (imsi < Sres(dp[i])) dp[i] = dp[i - 2] + "{}";
-
-		imsi = Sres("{}" + dp[i - 2]);
-		if (imsi < Sres(dp[i])) dp[i] = "{}" + dp[i - 2];
-
-		imsi = Sres(dp[i - 3] + "[]");
-		if (imsi < Sres(dp[i])) dp[i] = dp[i - 3] + "[]";
-
-		imsi = Sres("[]" + dp[i - 3]);
-		if (imsi < Sres(dp[i])) dp[i] = "[]" + dp[i - 3];
+		if (i % 2 == 0) dp[i] = compare(dp[i], "1" + dp[i / 2] + "2");
+		if (i % 3 == 0) dp[i] = compare(dp[i], "3" + dp[i / 3] + "4");
+		if (i % 5 == 0) dp[i] = compare(dp[i], "5" + dp[i / 5] + "6");
+		for (j = 1; j < i; j++) dp[i] = compare(dp[i], dp[j] + dp[i - j]);
 	}
-	while (t--) {
-		int x;
-		cin >> x;
-		cout << dp[x] << '\n';
+	while(t--) {
+		scanf("%d", &n);
+		for (i = 0; i < dp[n].size(); i++) printf("%c", d_s[dp[n][i] - '0']);
+		printf("\n");
+
 	}
 	return 0;
 }

0105dc3c71b99a22a642c4c52741f50affc8e379
FandPROG <88233473+FandPROG@users.noreply.github.com>
2024-07-27 17:07:06 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,28 +1,26 @@
 #include <stdio.h>
+#include <algorithm>
 
 int n, m;
-int d[10], check[10];
+int ans[10];
 
-void MNM(int N, int s) {
-    int i, a;
-    if(N == m) {
-		for(i=0;i<m;i++) {
-			printf("%d ", d[i]);
-		}
+void DFS(int x, int l) {
+	if (l == m) {
+		for (int i = 0; i < l; i++) printf("%d ", ans[i]);
 		printf("\n");
 		return;
 	}
-    for(i=s;i<=n;i++) {
-        if(check[i] == 0) {
-            check[i] = 1;
-            d[N] = i;    
-            MNM(N+1, i+1);
-            check[i] = 0;
-        }
-    }
+	else if (x == n + 1) return;
+	ans[l] = x;
+	DFS(x + 1, l + 1);
+	DFS(x + 1, l);
+	return;
 }
-int main() {
-    scanf("%d %d", &n, &m);
-    MNM(0, 1);
-    return 0;
+
+int main()
+{
+	int i, j;
+	scanf("%d %d", &n, &m);
+	DFS(1, 0);
+	return 0;
 }

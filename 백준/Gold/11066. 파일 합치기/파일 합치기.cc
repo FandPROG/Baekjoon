4f0dfbcc8efe100e8a36c0c213dbe13cd1b3bb18
FandPROG <88233473+FandPROG@users.noreply.github.com>
2024-09-09 09:39:23 +0900

    [Gold III] Title: 파일 합치기, Time: 88 ms, Memory: 2132 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.cc
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -1,45 +1,40 @@
 #include <stdio.h>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
 int dp[510][510];
+int arr[510];
 int sum[510];
 
 int main()
 {
-	int t;
-	scanf("%d", &t);
-	while (t--) {
-		int i, j, n;
-		vector <int> vec;
-		scanf("%d", &n);
-		for (i = 0; i < n; i++) {
-			int x;
-			scanf("%d", &x);
-			vec.push_back(x);
-		}
-		sum[0] = vec[0];
-		for (i = 1; i < n; i++) sum[i] = sum[i - 1] + vec[i];
-		for (i = 0; i < n; i++) {
-			for (j = 0; j < n; j++) {
-				dp[i][j] = 2e9;
-			}
-		}
-		for (i = 0; i < n; i++) dp[i][i] = 0;
-		for (i = 1; i < n; i++) {
-			for (j = 0; i + j < n; j++) {
-				int s = j;
-				int e = i + j;
-				for (int k = s; k < e; k++) {
-					int ssm;
-					if (s == 0) ssm = 0;
-					else ssm = sum[s - 1];
-					dp[s][e] = min(dp[s][e], dp[s][k] + dp[k + 1][e] + sum[e] - ssm);
-				}
-			}
-		}
-		printf("%d\n", dp[0][n - 1]);
-	}
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int i, j;
+        int k;
+        scanf("%d", &k);
+        for(i=1;i<=k;i++) {
+            scanf("%d", &arr[i]);
+            sum[i] = sum[i - 1] + arr[i];
+        }
+        for(i=1;i<=k;i++) {
+            for(j=1;j<=k;j++) {
+                if(i == j) continue;
+                dp[i][j] = 2e9;
+            }
+        }
+        for(i=1;i<k;i++) {
+            for(j=1;i + j <= k;j++) {
+                int s = j;
+                int e = i + j;
+                for(int m = s;m<e;m++) {
+                    dp[s][e] = min(dp[s][e], dp[s][m] + dp[m + 1][e] + sum[e] - sum[s - 1]);
+                }
+            }
+        }
+        printf("%d\n", dp[1][k]);
+    }    
+    return 0;
 }

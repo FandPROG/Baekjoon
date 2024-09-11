24670d48229394975c5e41149fc97bb4161d78f9
FandPROG <88233473+FandPROG@users.noreply.github.com>
2024-09-11 12:32:26 +0900

    [Gold IV] Title: 우체국, Time: 48 ms, Memory: 2776 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.cc
+++ 백준/Gold/2141. 우체국/우체국.cc
@@ -1,34 +1,39 @@
 #include <stdio.h>
 #include <algorithm>
 #include <vector>
-#include <cmath>
 
 using namespace std;
+using ll = long long;
 
-long long n;
-long long sum, L, R;
-vector <pair<long long, long long>> vec;
-long long ans = 2e19;
-long long ans_idx;
+int n;
+vector <pair<int, int>> vec;
 
-int main()
+int main() 
 {
-	long long i, j;
-	scanf("%lld", &n);
-	for (i = 0; i < n; i++) {
-		long long x, y;
-		scanf("%lld %lld", &x, &y);
-		vec.push_back({ x, y });
-		sum += y;
-	}
-	sort(vec.begin(), vec.end());
-	for (i = 0; i < n; i++) {
-		R = sum - L - vec[i].second;
-		long long res = abs(R - L);
-		if (res < ans) ans = res, ans_idx = i;
-		L += vec[i].second;
-	}
-	ans_idx = vec[ans_idx].first;
-	printf("%lld", ans_idx);
-	return 0;
+    int i, j;
+    scanf("%d", &n);
+    ll ans = 0;
+    for(i=0;i<n;i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        vec.push_back({x, y});
+        ans += y;
+    }
+    sort(vec.begin(), vec.end());
+    ll res;
+    if(ans % 2) {
+        res = (ans + 1) / 2;
+    }
+    else {
+        res = ans / 2;
+    }
+    ll now = 0;
+    for(i=0;i<n;i++) {
+        now += vec[i].second;
+        if(now >= res) {
+            printf("%d", vec[i].first);
+            return 0;
+        }
+    }
+    return 0;
 }

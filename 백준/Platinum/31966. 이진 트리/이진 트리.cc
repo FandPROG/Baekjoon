c5e924be987eea5fd03b73bdb4c5498a92db3a02
FandPROG <88233473+FandPROG@users.noreply.github.com>
2024-12-29 17:53:31 +0900

    [Platinum IV] Title: 이진 트리, Time: 56 ms, Memory: 4236 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/31966. 이진 트리/이진 트리.cc
+++ 백준/Platinum/31966. 이진 트리/이진 트리.cc
@@ -2,14 +2,15 @@
 #include <algorithm>
 
 using namespace std;
+using ll = long long;
 
-int fdp[100010];
-int edp[100010];
-int dp[100010];
-int Tsize[100010];
-int N;
+ll fdp[100010];
+ll edp[100010];
+ll dp[100010];
+ll Tsize[100010];
+ll N;
 
-int MOD = 1000000007;
+ll MOD = 1000000007;
 
 int main()
 {
@@ -17,16 +18,16 @@ int main()
     fdp[0] = 1;
     edp[0] = 1;
     Tsize[0] = 1;
-    scanf("%d", &N);
-    for(int i=1;i<=N;i++) {
-        int x, y;
-        scanf("%d %d", &x, &y);
-        int n = i;
+    scanf("%lld", &N);
+    for(ll i=1;i<=N;i++) {
+        ll x, y;
+        scanf("%lld %lld", &x, &y);
+        ll n = i;
         Tsize[n] = (Tsize[x] + Tsize[y]) % MOD;
         fdp[n] = (fdp[x] + ((fdp[y] + Tsize[y]) % MOD) - 1) % MOD;
         edp[n] = (edp[y] + ((edp[x] + Tsize[x]) % MOD) - 1) % MOD;
         dp[n] = ((((dp[x] + dp[y]) % MOD) + ((Tsize[y] * edp[x]) % MOD)) % MOD + ((Tsize[x] * fdp[y]) % MOD)) % MOD - 1;
     }
-    for(int i=1;i<=N;i++) printf("%d\n", dp[i] % MOD);
+    for(ll i=1;i<=N;i++) printf("%lld\n", dp[i] % MOD);
     return 0;
 }

65226c88815c122411b3e5f30a4733cf767b8aca
FandPROG <88233473+FandPROG@users.noreply.github.com>
2025-01-06 17:21:31 +0900

    [Platinum I] Title: 센터가 돋보여야 해, Time: 560 ms, Memory: 63772 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/24915. 센터가 돋보여야 해/센터가 돋보여야 해.cc
+++ 백준/Platinum/24915. 센터가 돋보여야 해/센터가 돋보여야 해.cc
@@ -5,86 +5,90 @@
 using namespace std;
 using ll = long long;
 
-struct Nd{
-    ll mx, mn, Lans, Rans, ans;
-    Nd() {
-        mx = Lans = Rans = ans = -2e11;
-        mn = 2e11;
+ll inf = (ll)1e9 * (ll)1e9 + 7;
+
+struct st {
+    ll LtoM; // 왼쪽에서 중간으로 최대
+    ll MtoR; // 중간에서 오른쪽으로 최대
+    ll mn;   // 구간 내 최소값
+    ll mx;   // 구간 내 최대값
+    ll ans;  // 전체 구간에서의 최적값
+
+    st() {
+        LtoM = MtoR = -inf;
+        mn = inf;
+        mx = -inf;
+        ans = -inf;
     }
+
     void set(ll x) {
-        mx = mn = x;
-        ans = Lans = Rans = -2e11;
+        LtoM = MtoR = -inf;
+        mn = mx = x;
+        ans = -inf;
+    }
+
+    const st operator+(const st &K) {
+        st R;
+        R.LtoM = max({LtoM, K.LtoM, K.mx - mn});
+        R.MtoR = max({MtoR, K.MtoR, mx - K.mn});
+        R.mx = max(mx, K.mx);
+        R.mn = min(mn, K.mn);
+        R.ans = max({ans, K.ans, K.MtoR - mn, LtoM - K.mn});
+        return R;
     }
 };
 
-Nd Calcu(Nd left, Nd right)
-{
-    Nd res;
-    res.mx = max(left.mx, right.mx);
-    res.mn = min(left.mn, right.mn);
-    res.Rans = max(right.Rans, right.mx - left.mn);
-    res.Lans = max(left.Lans, left.mx - right.mn);
-    res.ans = max(left.ans, max(right.ans, max(left.Rans - right.mn, right.Lans - left.mn)));
-    return res;
-}
-
 int n;
-ll arr[350000];
-Nd tree[1500000];
+ll arr[353333];
+st tree[1533333];
 
-void init(int s, int e, int nd)
-{
-    if(s == e) {
+void init(int s, int e, int nd) {
+    if (s == e) {
         tree[nd].set(arr[s]);
         return;
     }
     int mid = (s + e) / 2;
     init(s, mid, nd * 2);
     init(mid + 1, e, nd * 2 + 1);
-    tree[nd] = Calcu(tree[nd * 2], tree[nd * 2 + 1]);
+    tree[nd] = tree[nd * 2] + tree[nd * 2 + 1];
 }
 
-void Update(int s, int e, int nd, int idx, ll val)
-{
-    if(idx < s || idx > e) return;
-    if(s == e) {
+void Update(int s, int e, int nd, int idx, ll val) {
+    if (idx < s || idx > e) return;
+    if (s == e) {
         tree[nd].set(val);
         return;
     }
     int mid = (s + e) / 2;
     Update(s, mid, nd * 2, idx, val);
     Update(mid + 1, e, nd * 2 + 1, idx, val);
-    tree[nd] = Calcu(tree[nd * 2], tree[nd * 2 + 1]);
+    tree[nd] = tree[nd * 2] + tree[nd * 2 + 1];
 }
 
-Nd getans(int s, int e, int nd, int l, int r)
-{
-    if(l > e || s > r) return Nd();
-    if(l <= s && e <= r) return tree[nd];
+st getans(int s, int e, int nd, int l, int r) {
+    if (l > e || s > r) return st();
+    if (l <= s && e <= r) return tree[nd];
     int mid = (s + e) / 2;
-    return Calcu(getans(s, mid, nd * 2, l, r), getans(mid + 1, e, nd * 2 + 1, l, r));
+    return getans(s, mid, nd * 2, l, r) + getans(mid + 1, e, nd * 2 + 1, l, r);
 }
 
-int main()
-{
-    int i, j;
+int main() {
     int Q;
     scanf("%d %d", &n, &Q);
-    for(i=1;i<=n;i++) scanf("%lld", &arr[i]);
+    for (int i = 1; i <= n; i++) scanf("%lld", &arr[i]);
     init(1, n, 1);
-    while(Q--) {
+    while (Q--) {
         int x;
         scanf("%d", &x);
-        if(x == 1) {
+        if (x == 1) {
             int idx;
             ll val;
             scanf("%d %lld", &idx, &val);
             Update(1, n, 1, idx, val);
-        }
-        else {
+        } else {
             int l, r;
             scanf("%d %d", &l, &r);
-            Nd res = getans(1, n, 1, l, r);
+            st res = getans(1, n, 1, l, r);
             printf("%lld\n", res.ans);
         }
     }
